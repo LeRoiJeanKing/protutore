6bf46a116464343cab453a6fc7e5ae6521617334
theom <theomarechal24@gmail.com>
2024-01-24 11:57:22 +0100

    events

---
+++ pages/events.js
@@ -0,0 +1,14 @@
+// Import des modules nécessaires
+import React from 'react';
+
+// Page Events
+const EventsPage = () => {
+  return (
+    <div>
+      <h1>Liste des Événements</h1>
+      {/* Ajoute ici la logique pour afficher la liste des événements */}
+    </div>
+  );
+};
+
+export default EventsPage;
