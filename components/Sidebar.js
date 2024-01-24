933360e2da2c4348ba13946fd4717961dc669ce7
theom <theomarechal24@gmail.com>
2024-01-24 17:29:26 +0100

    login

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,12 +1,24 @@
 
-import React from 'react';
+// components/Sidebar.js
+import React, { useState } from 'react';
 import { useRouter } from 'next/router';
+import Modal from './Modal';
 
 const Sidebar = () => {
   const router = useRouter();
 
   const handleNavigation = (path) => {
     router.push(path);
+  };
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
   };
 
   return (
@@ -50,7 +62,32 @@ const Sidebar = () => {
       >
         Événements
       </button>
-      {/* Ajoute d'autres boutons pour les sections supplémentaires */}
+
+      {/* Bouton pour ouvrir la fenêtre modale de login */}
+      <button
+        style={{
+          display: 'block',
+          marginBottom: '10px',
+          color: '#fff',
+          backgroundColor: '#555',
+          border: 'none',
+          padding: '8px 16px',
+          cursor: 'pointer',
+          width: '100%',
+        }}
+        onClick={openModal}
+      >
+        Login
+      </button>
+
+      {/* Fenêtre modale pour le login */}
+      {isModalOpen && (
+        <Modal onClose={closeModal}>
+          {/* Contenu de la fenêtre modale */}
+          <h2>Login</h2>
+          {/* Ajoute les champs pour le pseudo et le mot de passe ici */}
+        </Modal>
+      )}
     </div>
   );
 };
