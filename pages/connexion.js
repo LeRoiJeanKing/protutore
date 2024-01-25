40f1c6c803c752abf086bd2b0cd0ac2a22f67904
theom <theomarechal24@gmail.com>
2024-01-25 11:33:32 +0100

    couscous

--- pages/connexion.js
+++ pages/connexion.js
@@ -5,6 +5,7 @@ import Sidebar from '../components/Sidebar';
 
 const ConnexionPage = () => {
   const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+  const [isRegistrationModalOpen, setIsRegistrationModalOpen] = useState(false);
 
   const openLoginModal = () => {
     setIsLoginModalOpen(true);
@@ -12,6 +13,14 @@ const ConnexionPage = () => {
 
   const closeLoginModal = () => {
     setIsLoginModalOpen(false);
+  };
+
+  const openRegistrationModal = () => {
+    setIsRegistrationModalOpen(true);
+  };
+
+  const closeRegistrationModal = () => {
+    setIsRegistrationModalOpen(false);
   };
 
   return (
@@ -22,6 +31,7 @@ const ConnexionPage = () => {
       <button
         style={{
           marginTop: '20px',
+          marginRight: '10px',
           padding: '8px 16px',
           cursor: 'pointer',
         }}
@@ -30,8 +40,28 @@ const ConnexionPage = () => {
         Login
       </button>
 
+      {/* Bouton pour ouvrir la fenêtre modale d'inscription */}
+      <button
+        style={{
+          marginTop: '20px',
+          padding: '8px 16px',
+          cursor: 'pointer',
+        }}
+        onClick={openRegistrationModal}
+      >
+        Inscription
+      </button>
+
       {/* Fenêtre modale de login */}
       <LoginModal isOpen={isLoginModalOpen} onClose={closeLoginModal} />
+
+      {/* Fenêtre modale d'inscription */}
+      <LoginModal isOpen={isRegistrationModalOpen} onClose={closeRegistrationModal}>
+        {/* Contenu de la fenêtre modale d'inscription */}
+        <h2>Inscription</h2>
+        {/* Ajoute les champs pour le pseudo, le mot de passe, etc. ici */}
+        <button onClick={closeRegistrationModal}>Fermer</button>
+      </LoginModal>
     </div>
   );
 };
